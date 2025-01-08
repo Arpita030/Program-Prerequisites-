714df98dbfc9bda94a0020668d82e220ff675a6e
Arpita030 <0111as211013.arpita@gmail.com>
2025-01-08 11:53:02 +0530

    question10

---
+++ conversion.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Conversion{
+	public static void main(String args[]){
+		Scanner sc= new Scanner(System.in);
+		double kilometers= sc.nextDouble();
+		System.out.println(kilometers*0.621371);
+	}
+}
